bf09613a6e236285ded8e9ae3825bbf643fa443d
MLAQTS <a0.a0.aa.0a.0a@gmail.com>
2016-02-27 15:37:48 -0500

    Added .java creation functionality

--- GUI/src/main/block/Switch.java
+++ GUI/src/main/block/Switch.java
@@ -7,12 +7,8 @@ import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.RenderingHints;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 
-import javax.swing.JButton;
 import javax.swing.JTextArea;
 
 import main.util.Controller;
